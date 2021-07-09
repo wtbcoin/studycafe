1297d9532d6436f8b29aebb1fc5479fb5afa48b0
wtbcoin <rnn3473@gmail.com>
2021-07-09 13:30:55 +0900

    ss

--- StudyCafeManagement/src/main/java/test/test.java
+++ StudyCafeManagement/src/main/java/test/test.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 	System.out.println("수정");
 	System.out.println("수정2");
 	System.out.println("수정3");
-	
+	System.out.println("수정4");
+	System.out.println("수정5");
 }
 }
