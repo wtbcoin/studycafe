0e32cad9ef0d3d5e7da09c8022df3627fff7ac04
DCU <DCU@203.250.35.119>
2021-07-09 13:28:12 +0900

    zzz

--- StudyCafeManagement/src/main/java/test/test.java
+++ StudyCafeManagement/src/main/java/test/test.java
@@ -5,6 +5,11 @@ public static void main(String[] args) {
 	System.out.println("수정");
 	System.out.println("수정2");
 	System.out.println("수정3");
+	System.out.println("수정4");
+	System.out.println("수정5");
+	System.out.println("수정6");
+	System.out.println("수정7");	
+	System.out.println("수정8");
 	
 }
 }
