951c644a92c6b395b850e893c568c4340b5f5d8b
DCU <DCU@203.250.35.164>
2021-07-15 11:04:11 +0900

    sql

---
+++ StudyCafeManagement/sql/User.sql
@@ -0,0 +1,20 @@
+CREATE TABLE User(
+	user_id varchar2(20) primary key not null,
+	user_password varchar2(20) not null,
+	user_name varchar2(20) not null,
+	user_gender char(1) not null,
+	user_phone char(11) not null,
+	user_address varchar2(40) null,
+	
+	
+)
+
+
+CREATE TABLE member1(
+	id NUMBER PRIMARY KEY,
+	name VARCHAR2(20),
+	email  VARCHAR2(30),
+	phone VARCHAR2(20)
+);
+
+CREATE SEQUENCE member1_seq;
